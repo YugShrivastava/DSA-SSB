6c68722e1e6f4f2c088760a56a6b81f4a51571da
Yug <shrivastavayug04@gmail.com>
2024-09-24 17:37:34 +0530

    Set-2, 6 programs implemented

--- Set-2.cpp
+++ Set-2.cpp
@@ -138,8 +138,58 @@ int main() {
     return 0;
 }
 */
+/*
+int main(){
+    int size = 6, Sum, flag = 0;
+    int arr[size] = {1, 2, 4, 5, 7, 11};
+    cout << "Enter Sum: ";
+    cin >> Sum;
+
+    for(int i = 0; i < size; i++){
+        for(int j = i+1; j < size; j++){
+            if(arr[i] + arr[j] == Sum){
+                cout << "Numbers are: " << arr[i] << " and " << arr[j] << endl;
+                flag = 1;
+            }
+        }
+    }
+    if(flag != 1){
+        cout << "No such numbers found...";
+    }
+}
+*/
+/*
+int main(){
+    int size = 6, currSum = 0, Sum, flag = 0;
+    int arr[size] = {1, 2, 4, 5, 7, 11};
+
+    cout << "Enter Sum: ";
+    cin >> Sum;
+
+    int i = 0, j = size-1;
+    while(i < j){
+        if(arr[i] + arr[j] > Sum){
+            j--;
+        }
+        else if(arr[i] + arr[j] < Sum){
+            i++;
+        }
+        else{
+            cout << "Numbers are: " << arr[i] << " and " << arr[j] << endl;
+            flag = 1;
+            i++;
+            j--;
+        }
+    }
+
+    if(flag != 1){
+        cout << "No such numbers found...";
+    }
+}
+*/
 
 int main(){
-    
+        
 
+    return 0;
 }

71d8b1978111befb40f47eafa3156e2963d3e36a
Yug <shrivastavayug04@gmail.com>
2024-10-14 16:32:10 +0530

    Heap insertion implemented

--- heap.cpp
+++ heap.cpp
@@ -1,42 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int size = 11;
     int heap[size], index, element;
 
-//  Insert heap
-    while(index < size){
+    //  Insert heap
+    while (index < size)
+    {
         int element;
         cout << "Enter element: ";
         cin >> element;
 
-        if(index == 0){
+        if (index == 0)
+        {
             heap[index] = element;
             index++;
         }
 
-        else if(heap[(index-1) / 2] < element){
+        else if (heap[(index - 1) / 2] < element)
+        {
             int indexCpy = index;
-            while(heap[(indexCpy-1) / 2] < element){
-                int temp = heap[(indexCpy-1)/2];
-                heap[(indexCpy-1)/2] = element;
+            while (heap[(indexCpy - 1) / 2] < element)
+            {
+                int temp = heap[(indexCpy - 1) / 2];
+                heap[(indexCpy - 1) / 2] = element;
                 element = temp;
                 heap[indexCpy] = element;
-                indexCpy = (indexCpy-1)/2;
+                indexCpy = (indexCpy - 1) / 2;
                 element = heap[indexCpy];
             }
             index++;
         }
 
-        else{
+        else
+        {
             heap[index] = element;
             index++;
         }
     }
 
-//  Print heap
-    for(int i = 0; i < size; i++){
+    //  Print heap
+    for (int i = 0; i < size; i++)
+    {
         cout << heap[i] << " ";
     }
 }
+
